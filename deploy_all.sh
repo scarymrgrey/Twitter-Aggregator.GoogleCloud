44314f6d6830d83593bc0e38e3142d41430c5ff0
Kirill Polunin <kpolunin@griddynamics.com>
2021-03-29 15:43:40 +0200

    fixed scripts

--- deploy_all.sh
+++ deploy_all.sh
@@ -11,6 +11,6 @@ zip -r -j ./src/http-server/artifact.zip ./src/http-server
 gsutil cp ./src/agg/artifact.zip gs://${BUCKET_NAME}/agg/
 gsutil cp ./src/consumer/artifact.zip gs://${BUCKET_NAME}/consumer/
 gsutil cp ./src/http-server/artifact.zip gs://${BUCKET_NAME}/http-server/
-gcloud deployment-manager deployments create fedex-twitter-deployment-new --config deployment.yaml
-gcloud dataflow jobs run pubsub2bq --gcs-location gs://dataflow-templates/latest/PubSub_Subscription_to_BigQuery --staging-location gs://${BUCKET_NAME}/staging --parameters inputSubscription=projects/${PROJECT_NAME}/subscriptions/pubsub2bq,outputTableSpec=${PROJECT_NAME}:tweetsds.tweets
-gcloud functions call consume_feed --data "{\"data\":\"CONSUME_FEED\"}"
+gcloud deployment-manager deployments create fedex-twitter-deployment --config deployment.yaml
+gcloud dataflow jobs run pubsub2bq --region europe-west1 --gcs-location gs://dataflow-templates/latest/PubSub_Subscription_to_BigQuery --staging-location gs://${BUCKET_NAME}/staging --parameters inputSubscription=projects/${PROJECT_NAME}/subscriptions/pubsub2bq,outputTableSpec=${PROJECT_NAME}:tweetsds.tweets
+gcloud functions call consume_feed --region europe-west1 --data  "{\"data\":\"CONSUME_FEED\"}"
