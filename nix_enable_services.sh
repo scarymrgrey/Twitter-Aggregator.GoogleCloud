fa11d7d7b6695a2e0b9420c3053c579b90b39cef
kpolunin <kiri.polunin@gmail.com>
2021-03-29 10:30:32 +0100

    region

--- nix_enable_services.sh
+++ nix_enable_services.sh
@@ -3,3 +3,4 @@ gcloud services enable compute.googleapis.com
 gcloud services enable dataflow.googleapis.com
 gcloud services enable cloudbuild.googleapis.com
 gcloud services enable cloudfunctions.googleapis.com
+gcloud compute project-info add-metadata --metadata google-compute-default-region=europe-west1
